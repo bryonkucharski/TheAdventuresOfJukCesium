87c5bcb728f22c4e0d732e35b32d27b37c781ed1
hickeym2 <hickeym2@wit.edu>
2016-11-14 21:44:29 -0500

    Added clipping, sprite movement, sprinting, WASD
    modified Player.cpp, World.cpp

--- SFML_RPG/SFML_RPG/Player.cpp
+++ SFML_RPG/SFML_RPG/Player.cpp
@@ -6,6 +6,12 @@ Player::~Player() {
 //handles player movements. called every frame in main.
 void Player::updatePlayer(RenderWindow &window) {
 
+	//implement spriting by holding shift, MH
+	int speedMod = 0;
+	if (Keyboard::isKeyPressed(Keyboard::LShift) || Keyboard::isKeyPressed(Keyboard::RControl)) {
+		speedMod = 5;
+	}
+
 	//allow player to walk at the start of every frame. 
 	this->setAllWalk(true);
 
@@ -21,46 +27,64 @@ void Player::updatePlayer(RenderWindow &window) {
 	//increment walking counter for animation 
 	this->setWalkingCounter(this->getWalkingCounter() + 1);
 
-	if (Keyboard::isKeyPressed(Keyboard::Left))
+	//added WASD support MH
+	if (Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A))
 	{
 		this->setDirection(1);
-		this->getSprite().setTextureRect(IntRect(this->getWalkingCounter() * 32, 96, 32, 32));
+		//Fixed all the walking animations, MH
+		this->getSprite().setTextureRect(IntRect(this->getWalkingCounter() * 32, 32, 32, 32));
 		this->setCanWalkLeft(!this->checkForIntersect(obstacles, this->getRect()));
 
 		if (this->isWalkLeftAllowed()) {
-			this->getRect().move(-this->getSpeed(), 0);
+			//if you are not on an obstacle, you can walk.
+			this->getRect().move(-this->getSpeed() - speedMod, 0);
+		}
+		else {
+			//attempting to fix collison that makes you stuck.
+			//this is technically saying if you are in an obstacle, walk backwards.
+			//How can we say, never enter an obstacle
+			//We have to tweak how intersects work then.
+			//of if we have interected, setPosition(previous, previous)
+			this->getRect().move(this->getSpeed() + speedMod + 2, 0);
 		}
 	}
-	else if (Keyboard::isKeyPressed(Keyboard::Right))
+	else if (Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D))
 	{
 		this->setDirection(2);
-		this->getSprite().setTextureRect(IntRect(this->getWalkingCounter() * 32, 32, 32, 32));
+		this->getSprite().setTextureRect(IntRect(this->getWalkingCounter() * 32, 32*2, 32, 32));
 		this->setCanWalkRight(!this->checkForIntersect(obstacles, this->getRect()));
 
 		//only move player if allowed to walk (no intersect)
 		if (this->isWalkRightAllowed()) {
-			this->getRect().move(this->getSpeed(), 0);
+			this->getRect().move(this->getSpeed() + speedMod, 0);
+		}else {
+			this->getRect().move(-this->getSpeed() - speedMod - 2, 0);
 		}
 	}
-	else if (Keyboard::isKeyPressed(Keyboard::Up))
+	else if (Keyboard::isKeyPressed(Keyboard::Up) || Keyboard::isKeyPressed(Keyboard::W))
 	{
 		this->setDirection(3);
-		this->getSprite().setTextureRect(IntRect(this->getWalkingCounter() * 32, 64, 32, 32));
+		this->getSprite().setTextureRect(IntRect(this->getWalkingCounter() * 32, 96, 32, 32));
 		this->setCanWalkUp(!this->checkForIntersect(obstacles, this->getRect()));
 			
 		//only move player if allowed to walk (no intersect)
 		if (this->isWalkUpAllowed()) {
-			this->getRect().move(0, -this->getSpeed());
+			this->getRect().move(0, -this->getSpeed() - speedMod);
+		}else {
+			this->getRect().move(0, this->getSpeed() + speedMod + 2);
 		}
 	}
-	else if (Keyboard::isKeyPressed(Keyboard::Down))
+	else if (Keyboard::isKeyPressed(Keyboard::Down) || Keyboard::isKeyPressed(Keyboard::S))
 	{
 		this->setDirection(4);
 		this->getSprite().setTextureRect(IntRect(this->getWalkingCounter() * 32, 0, 32, 32));
 		this->setCanWalkDown(!this->checkForIntersect(obstacles, this->getRect()));
 
 		if (this->isWalkDownAllowed()) {
-			this->getRect().move(0, this->getSpeed());
+			this->getRect().move(0, this->getSpeed() + speedMod);
+		}else {
+			//this->getRect().setPosition(/*The last X position before intersection*/ , /*The last Y position before intersection - speedMod - 2*/ )
+			this->getRect().move(0, -this->getSpeed() - speedMod - 2);
 		}
 	}
 
--- SFML_RPG/SFML_RPG/World.cpp
+++ SFML_RPG/SFML_RPG/World.cpp
@@ -40,10 +40,38 @@ void World::setupHome(RenderWindow &window) {
 	trees.setPosition(Vector2f(PIXEL_SIZE * 19, 0));
 	trees.setFillColor(Color::Transparent);
 
-	RectangleShape bigHouse(Vector2f(10 * PIXEL_SIZE, 5 * PIXEL_SIZE));
-	trees.setPosition(Vector2f(PIXEL_SIZE * 19, 0));
-	trees.setFillColor(Color::Transparent);
+	//Building the big house MH
+	RectangleShape bigHouse1(Vector2f(11 * PIXEL_SIZE, 9 * PIXEL_SIZE));
+	bigHouse1.setPosition(Vector2f(PIXEL_SIZE * 5, PIXEL_SIZE * 9));
+	bigHouse1.setFillColor(Color::Transparent);
+	RectangleShape bigHouse2(Vector2f(16 * PIXEL_SIZE, 9 * PIXEL_SIZE));
+	bigHouse2.setPosition(Vector2f(PIXEL_SIZE * 0, PIXEL_SIZE * 7));
+	bigHouse2.setFillColor(Color::Transparent);
+	RectangleShape bigHouse3(Vector2f(17 * PIXEL_SIZE, 9 * PIXEL_SIZE));
+	bigHouse3.setPosition(Vector2f(PIXEL_SIZE * 0, PIXEL_SIZE * 6));
+	bigHouse3.setFillColor(Color::Transparent);
+	RectangleShape bigHouse4(Vector2f(18 * PIXEL_SIZE, 9 * PIXEL_SIZE));
+	bigHouse4.setPosition(Vector2f(PIXEL_SIZE * 0, PIXEL_SIZE * 7));
+	bigHouse4.setFillColor(Color::Transparent);
+	RectangleShape bigHouse5(Vector2f(19 * PIXEL_SIZE, 9 * PIXEL_SIZE));
+	bigHouse5.setPosition(Vector2f(PIXEL_SIZE * 5, PIXEL_SIZE * 9));
+	bigHouse5.setFillColor(Color::Transparent);
+	
+	RectangleShape menuBar(Vector2f(0 * PIXEL_SIZE, 20 * PIXEL_SIZE));//starting position. MH
+	menuBar.setPosition(Vector2f(PIXEL_SIZE * 30, PIXEL_SIZE * 2));//the dimensions of the rectangle. MH
+	menuBar.setFillColor(Color::Transparent);
 
+	//Add the shape to the obstacle list
 	home.addToObstacles(house);
 	home.addToObstacles(trees);
+	home.addToObstacles(menuBar);
+	
+	/*
+	//many obstacles make the player stuck. why?
+	home.addToObstacles(bigHouse1);
+	home.addToObstacles(bigHouse2);
+	home.addToObstacles(bigHouse3);
+	home.addToObstacles(bigHouse4);
+	home.addToObstacles(bigHouse5);
+	*/
 }
