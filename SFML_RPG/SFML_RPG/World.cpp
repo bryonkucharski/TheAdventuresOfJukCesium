8e3146abee8c79f734d2c88f16e3fd5aedcd7e92
hickeym2 <hickeym2@wit.edu>
2016-11-14 23:47:52 -0500

    Fixed BigHouse Obstacle
    big house obstacle

--- SFML_RPG/SFML_RPG/World.cpp
+++ SFML_RPG/SFML_RPG/World.cpp
@@ -41,37 +41,33 @@ void World::setupHome(RenderWindow &window) {
 	trees.setFillColor(Color::Transparent);
 
 	//Building the big house MH
-	RectangleShape bigHouse1(Vector2f(11 * PIXEL_SIZE, 9 * PIXEL_SIZE));
-	bigHouse1.setPosition(Vector2f(PIXEL_SIZE * 5, PIXEL_SIZE * 9));
+	RectangleShape bigHouse1(Vector2f(5 * PIXEL_SIZE, 9 * PIXEL_SIZE));
+	bigHouse1.setPosition(Vector2f(PIXEL_SIZE * 11, PIXEL_SIZE * 9));
 	bigHouse1.setFillColor(Color::Transparent);
-	RectangleShape bigHouse2(Vector2f(16 * PIXEL_SIZE, 9 * PIXEL_SIZE));
-	bigHouse2.setPosition(Vector2f(PIXEL_SIZE * 0, PIXEL_SIZE * 7));
+	RectangleShape bigHouse2(Vector2f(1 * PIXEL_SIZE, 7 * PIXEL_SIZE));
+	bigHouse2.setPosition(Vector2f(PIXEL_SIZE * 16, PIXEL_SIZE * 9));
 	bigHouse2.setFillColor(Color::Transparent);
-	RectangleShape bigHouse3(Vector2f(17 * PIXEL_SIZE, 9 * PIXEL_SIZE));
-	bigHouse3.setPosition(Vector2f(PIXEL_SIZE * 0, PIXEL_SIZE * 6));
+	RectangleShape bigHouse3(Vector2f(1 * PIXEL_SIZE, 6 * PIXEL_SIZE));
+	bigHouse3.setPosition(Vector2f(PIXEL_SIZE * 17, PIXEL_SIZE * 9));
 	bigHouse3.setFillColor(Color::Transparent);
-	RectangleShape bigHouse4(Vector2f(18 * PIXEL_SIZE, 9 * PIXEL_SIZE));
-	bigHouse4.setPosition(Vector2f(PIXEL_SIZE * 0, PIXEL_SIZE * 7));
+	RectangleShape bigHouse4(Vector2f(1 * PIXEL_SIZE, 7 * PIXEL_SIZE));
+	bigHouse4.setPosition(Vector2f(PIXEL_SIZE * 18, PIXEL_SIZE * 9));
 	bigHouse4.setFillColor(Color::Transparent);
-	RectangleShape bigHouse5(Vector2f(19 * PIXEL_SIZE, 9 * PIXEL_SIZE));
-	bigHouse5.setPosition(Vector2f(PIXEL_SIZE * 5, PIXEL_SIZE * 9));
+	RectangleShape bigHouse5(Vector2f(5 * PIXEL_SIZE, 9 * PIXEL_SIZE));
+	bigHouse5.setPosition(Vector2f(PIXEL_SIZE * 19, PIXEL_SIZE * 9));
 	bigHouse5.setFillColor(Color::Transparent);
 	
-	RectangleShape menuBar(Vector2f(0 * PIXEL_SIZE, 20 * PIXEL_SIZE));//starting position. MH
-	menuBar.setPosition(Vector2f(PIXEL_SIZE * 30, PIXEL_SIZE * 2));//the dimensions of the rectangle. MH
-	menuBar.setFillColor(Color::Transparent);
 
 	//Add the shape to the obstacle list
 	home.addToObstacles(house);
 	home.addToObstacles(trees);
-	home.addToObstacles(menuBar);
 	
-	/*
+	
 	//many obstacles make the player stuck. why?
 	home.addToObstacles(bigHouse1);
 	home.addToObstacles(bigHouse2);
 	home.addToObstacles(bigHouse3);
 	home.addToObstacles(bigHouse4);
 	home.addToObstacles(bigHouse5);
-	*/
+	
 }
